045588c455dbcfaeaa7637b9191f20693519d377
local <local@dummy.co.jp>
2015-11-12 18:30:07 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+/* 必須 */
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+/* サーバ関係 */
+var browserSync = require('browser-sync').create();
+var ssi         = require('browsersync-ssi');
+
+/* jsの結合を行う場合に必要 */
+var browserify  = require('browserify');
+var buffer      = require('vinyl-buffer');
+var source      = require('vinyl-source-stream');
+var glob        = require('glob');
+
+
+
+/********************************
+ * path定義
+ * - ROOT : ドキュメントルートのパス
+ * - CSS  : コンパイルするscssと出力するディレクトリ
+ * - JS   : コンパイルするjsと出力するディレクトリ
+ * - WATCH: ライブリロード用の監視ファイル
+ * - GUIDE: スタイルガイドを出力するディレクトリ
+ ********************************/
+var ROOT = './root/';
+var CSS = {
+  SRC     : ROOT + 'common/_src/scss/**/*.scss',
+  SRC_DIR : ROOT + 'common/_src/scss/',
+  DST_DIR : ROOT + 'common/css/',
+  DOC_ROOT: '/common/css/',
+};
+var JS = {
+  SRC     : ROOT + 'common/_src/js/**/*.js',
+  SRC_DIR : ROOT + 'common/_src/js/',
+  DST_DIR : ROOT + 'common/js/',
+  DOC_ROOT: 'common/js'
+};
+var WATCH = [
+  CSS.DST_DIR + '*.css',
+  JS.DST_DIR  + '*.js',
+  ROOT + '**/*.html'
+];
+var GUIDE =  ROOT + '_guide/';
+
+
+/********************************
+ * task定義
+ * - server : 静的サーバの構築(SSI付き)
+ * - sass   : sassファイルのコンパイル（autoprefix, csscomb）
+ * - guide  : スタイルガイドの出力
+ * - js     : browserifyとuglifyを使った結合
+ * - watch  : ライブリロード（使用しないものは無効に）
+ * - default: 使用しないものは無効に
+ * - del_   : コンパイルされたファイルを全て削除する
+ ********************************/
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: ROOT,
+      directory: false,
+      middleware: [
+        ssi({
+          baseDir: __dirname + '/' + ROOT,  // サーバからのパスに注意
+          ext: ".html"
+        })
+      ]
+    },
+    open: false,
+    port: 9999
+  });
+});
+
+
+gulp.task('sass', function () {
+  return $.rubySass(CSS.SRC, {
+      style: 'expanded',
+      stopOnError: false,
+      cacheLocation: './sass-cache'
+    })
+    .pipe($.plumber())
+    .pipe($.autoprefixer({
+        browsers: ['last 2 versions', 'ie 9'],
+        cascade: false
+    }))
+    .pipe($.csscomb())  // もし重たくなってきたら場合は別タスク化
+    .pipe(gulp.dest(CSS.DST_DIR))
+
+    // ガイド生成用に別途concatしてdestしておく
+    .pipe($.concat('style.css'))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest(GUIDE));  // publicの中だと上書きされてしまう
+});
+
+
+/* kss-nodeを利用したガイド生成 */
+gulp.task('guide', $.shell.task([
+    'kss-node <%= source %> <%= destination %> --template <%= template%> --css <%= cssfile %>'
+  ], {
+    templateData: {
+      source: CSS.SRC_DIR,  // ファイルを直接指定だと動かない
+      destination: GUIDE,
+      template: './kss-template/',
+      cssfile: CSS.DOC_ROOT + 'style.css'  // ドキュメントルートからの位置になることに注意
+    }
+  }
+));
+
+
+gulp.task('js', function(){
+  var srcFiles = glob.sync(JS.SRC);
+  return browserify({
+    entries: srcFiles
+  })
+  // エラーが出ても止めない
+  .bundle().on('error', function(err) {
+      console.log('ERROR! \n' + err.message);
+      this.emit('end');
+  })
+  .pipe(source('app.js'))  // 出力ファイル名
+  .pipe(buffer())  // uglifyする時には必須
+  // .pipe($.uglify())  // 圧縮（使用しない変数関数は出力しない。多分）
+  .pipe(gulp.dest(JS.DST_DIR));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(CSS.SRC, ['sass', 'guide']);  // guideが重たくなってきたら外す
+  gulp.watch(JS.SRC,  ['js']);
+  gulp.watch(WATCH).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['server', 'watch', 'sass', 'guide', 'js']);
+
+
+/* remove */
+gulp.task('del_css', function() {
+  return gulp.src([CSS.DST_DIR + '*.css'], { read: false })
+    .pipe($.rimraf({ force: true }));
+});
+gulp.task('del_js', function() {
+  return gulp.src([JS.DST_DIR + 'app.js'], { read: false })
+    .pipe($.rimraf({ force: true }));
+});
