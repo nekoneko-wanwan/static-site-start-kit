9033dc93c1942acf96e17ccaae669a4e187c21b5
local <local@dummy.co.jp>
2015-11-28 13:33:36 +0900

    module内を再整理

--- kss-template/public/styleguide.js
+++ kss-template/public/styleguide.js
@@ -1,6 +1,10 @@
 $(function(){
 	hljs.initHighlightingOnLoad();
 
-	var ref = $('.styleguide-menu').find('.styleguide-menu-list').data('kss-ref');
-	$('.styleguide-menu').find('a').eq(ref).addClass('selected');
+	var paths = window.location.pathname.split('/');
+	var path = paths[paths.length - 1];
+	$('.styleguide-menu').find('a[href="'+path+'"]').addClass('selected');
+
+	// var ref = $('.styleguide-menu').find('.styleguide-menu-list').data('kss-ref');
+	// $('.styleguide-menu').find('a').eq(ref).addClass('selected');
 });
