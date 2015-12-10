d865b85229c7b3504f67eaba111362181586c310
local <local@dummy.co.jp>
2015-12-10 15:18:56 +0900

    共通スクリプトのサンプルを再構成

--- root/common/js/app.js
+++ root/common/js/app.js
@@ -1,54 +1,33 @@
-// 全ページ共通で使用するスクリプト
-
 /**
- * グローバルナビゲーションの現在地表示
- * - グローバルナビゲーションの該当する項目にclassを付与
- * - 該当する項目とは、どの第1ディレクトリに属しているページがどうか
+ * 全ページ共通で使用するスクリプト
  */
-var setClassGnav = function() {
-  // 第1ディレクトリを取得（#以下を排除しておく）
-  var dir = location.href.split('/')[3].replace(/^(.*)#(.*)/,'$1');
 
-  // ホームかどうか
-  var isTopPage = function() {
-    var topFileNames = ['', 'index.html', 'index.htm', 'index.php', 'index'];
-    if (topFileNames.indexOf(dir) >= 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  // 該当する項目にclassを付与
-  var $nav = $('#js-gnav');
-  if (isTopPage()) {
-    $nav.find('a[href="/"]').addClass('is-active');
-  } else {
-    $nav.find('a[href="/'+dir+'/"]').addClass('is-active');
-  }
-};
+/* 変数一覧 */
+var index_list = ['', 'index.html', 'index.htm', 'index.php', 'index'];
 
 
 
 /**
- * サブナビゲーションの現在地表示
- * - サブナビゲーションの該当する項目にclassを付与
+ * 汎用的な関数
  */
-var setClassSnav = function() {
-  var $nav = $('#js-snav');
-
-  // サブナビが存在しなければ離脱
-  if ($nav.size() <= 0 ) { return;}
-
-  var getPathName = function() {
+var utility = {
+  /**
+   * getPathName: location.pathnameをカスタムして取得
+   * - カスタム1: 渡した文字列が含まれればそれを削除する（トップと見なす）
+   * - カスタム2: # 以下を削除する
+   *
+   * @param {array} index_list
+   * @return {str} 
+   */
+  getPathName: function(list) {
+    list = list || index_list;
     var path = location.pathname;
     var arr  = path.split('/');
     var lastPathName = arr.slice(arr.length - 1)[0];
 
     // indexかどうか
     var isIndexPage = function() {
-      var indexFileNames = ['index.html', 'index.htm', 'index.php', 'index'];
-      if (indexFileNames.indexOf(lastPathName) >= 0) {
+      if (list.indexOf(lastPathName) >= 0) {
         return true;
       } else {
         return false;
@@ -60,12 +39,68 @@ var setClassSnav = function() {
       path = path.replace(new RegExp(lastPathName, 'g'), '');
     }
     return path;
+  }
+};
+
+
+
+/**
+ * グローバルナビゲーションの現在地表示
+ * - グローバルナビゲーションの該当する項目にclassを付与
+ * - 該当する項目とは、どの第1ディレクトリに属しているページがどうか
+ *
+ * $param {jqueryObj} $elm: 対象とするナビのjqueryObjectを渡す
+ */
+var setClassGnav = function($elm) {
+  // 第1ディレクトリを取得（#以下を排除しておく）
+  var dir = location.href.split('/')[3].replace(/^(.*)#(.*)/,'$1');
+
+  // ホームかどうか
+  var isHomePage = function() {
+    return index_list.indexOf(dir) >= 0 ? true : false;
   };
 
   // 該当する項目にclassを付与
-  $nav.find('a[href="' + getPathName() + '"]').addClass('is-active');
+  if (isHomePage()) {
+    $elm.find('a[href="/"]').addClass('is-active');
+  } else {
+    $elm.find('a[href="/'+dir+'/"]').addClass('is-active');
+  }
 };
 
+
+
+/**
+ * サブナビゲーションの現在地表示
+ * - サブナビゲーションの該当する項目にclassを付与
+ *
+ * $param {jqueryObj} $elm: 対象とするナビのjqueryObjectを渡す
+ */
+var setClassSnav = function($elm) {
+  // サブナビが存在しなければ離脱
+  if ($elm.size() <= 0 ) { return;}
+
+  // 該当する項目にclassを付与
+  var pathName = utility.getPathName();
+  $elm.find('a[href="' + pathName + '"]').addClass('is-active');
+};
+
+
+
+/**
+ * バナーの重複削除
+ * - 現在表示しているページに該当するバナーを削除する
+ *
+ * $param {jqueryObj} $elm: 対象とするバナーのjqueryObjectを渡す
+ */
+var removeConflictBanner = function($elm) {
+  // バナーが存在しなければ離脱
+  if ($elm.size() <= 0 ) { return;}
+
+  // 該当する項目にclassを付与
+  var pathName = utility.getPathName();
+  $elm.find('a[href="' + pathName + '"]').parent('li').remove();
+};
 
 
 
@@ -118,8 +153,10 @@ var smoothScroll = function() {
 
 
 $(function() {
-  setClassGnav();
-  setClassSnav();
+  setClassGnav($('#js-gnav'));
+  setClassSnav($('#js-snav'));
+  removeConflictBanner($('#js-banner'));
   smoothScroll();
 });
+
 
